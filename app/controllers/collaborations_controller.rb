60068ad54d84363649dd1a3e4428ae2c2082e67d
JunXian <eldrethlim@gmail.com>
2014-07-20 10:20:52 +0000

    minor collab updates

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,10 +1,13 @@
 class CollaborationsController < ApplicationController
   def add_collaborators
     @wiki = current_user.wikis.find(params[:id])
+    name = @wiki.name
 
     if @wiki.add_collaborators.save(@wiki, user_id: params[:user_id])
-      redirect_to current_user
+      redirect_to edit_wiki_path, :notice => "Collaborators for #{name} updated."
     else
+      flash[:error] = "There was a problem updating the collaborators for #{name}. Please try again."
+      redirect_to edit_wiki_path
     end
   end
 
