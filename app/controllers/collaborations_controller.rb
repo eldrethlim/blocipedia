ed127e93ebd2942ef631c8aafcee294ec9c4cdf1
JunXian <eldrethlim@gmail.com>
2014-07-21 13:11:41 +0000

    removed old files

--- app/controllers/collaborations_controller.rb
+++
@@ -1,7 +0,0 @@
-class CollaborationsController < ApplicationController
-
-  def collaborate
-    Collaboration.update_all({:wiki_id}, {id: params[:user_ids]})
-    redirect_to edit_wiki_path
-  end
-end
